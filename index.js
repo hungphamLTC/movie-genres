d8951ff4f4b7c4941b087cfd136020580d8d1657
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-08-28 09:29:56 +0300

    Movies API endpoints

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: 'config/config.env' });
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const app = express();
 
 mongoose.set('strictQuery', false)
@@ -10,11 +11,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/movies')
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.error('Cound not connect to MongoDB'))
 
-
-
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers)
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening to ${port}`));
